489ff53dd7f8f9de867d989c48d70a43864fa8c9
wogal <egilgeous@gmail.com>
2024-10-26 19:02:08 -0400

    Sat 10/26/2024 - 7:01pm

--- GPSTrackingApp_100/app/src/main/java/com/example/gpstrackingapp_100/MainActivity.java
+++ GPSTrackingApp_100/app/src/main/java/com/example/gpstrackingapp_100/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     // wogal Earle -------////-------// --
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
