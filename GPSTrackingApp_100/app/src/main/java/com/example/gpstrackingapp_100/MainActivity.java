453d94c06c9f495384798cfdaa48df57f580cfe1
wogal <egilgeous@gmail.com>
2024-10-26 18:46:04 -0400

    Sat 10/26/2024 - 5:23pm

--- GPSTrackingApp_100/app/src/main/java/com/example/gpstrackingapp_100/MainActivity.java
+++ GPSTrackingApp_100/app/src/main/java/com/example/gpstrackingapp_100/MainActivity.java
@@ -8,9 +8,6 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-
-
-
 public class MainActivity extends AppCompatActivity {
 
     // wogal Earle -------////-------// --
