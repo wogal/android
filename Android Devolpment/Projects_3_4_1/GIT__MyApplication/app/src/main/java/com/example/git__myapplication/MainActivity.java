f1816bae7a6d1aa822b99798b2a38023d795c819
wogal <egilgeous@gmail.com>
2024-10-27 16:55:30 -0400

    Sun 10/27/2024 -- 4:45pm

--- Android Devolpment/Projects_3_4_1/GIT__MyApplication/app/src/main/java/com/example/git__myapplication/MainActivity.java
+++ Android Devolpment/Projects_3_4_1/GIT__MyApplication/app/src/main/java/com/example/git__myapplication/MainActivity.java
@@ -1,62 +1,50 @@
-package com.example.javafragmentsgooglemaps_new;
-
-
-import android.content.pm.PackageManager;
+package com.example.git__myapplication;
 
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
     public static final int Default_INTERVAL_MILLIS = 1000 * 30;
     public static final int Default_FastestInterval = 1000 * 5;
     private static final int PERMISSIONS_FINE_LOCATION = 69;
     TextView tv_labellat, tv_lat, tv_labellon, tv_lon, tv_labelaltitude, tv_altitude, tv_labelaccuracy, tv_accuracy, tv_labelspeed, tv_speed, tv_labelsensor, tv_sensor, tv_labelupdates, tv_updates, tv_address, tv_lbladdress;
     Switch sw_locationsupdates, sw_gps;
 
+    Button bu_TestCode;
+
     // Googles API for locations services
     FusedLocationProviderClient fusedLocationProviderClient;
     LocationRequest locationRequest;
 
+
     // wogal test start
     String test = "drtryhdu";
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
-        test = "new value uu7";
-
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
         // label allocations
         tv_labellat = findViewById(R.id.tv_labellat);
         tv_lat = findViewById(R.id.tv_lat);
@@ -77,17 +65,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // switch allocations
         sw_locationsupdates = findViewById(R.id.sw_locationsupdates);
         sw_gps = findViewById(R.id.sw_gps);
+        bu_TestCode = findViewById(R.id.bu_TestCode);
 
 
-        // set al properties of LocationRequest
+        // set all properties of LocationRequest
         locationRequest = new LocationRequest();
         locationRequest.setInterval(Default_INTERVAL_MILLIS);
         locationRequest.setFastestInterval(Default_FastestInterval);
         locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
 
 
-
         // set onclick listerners
+        bu_TestCode.setOnClickListener(this);
         sw_gps.setOnClickListener(this);
         sw_locationsupdates.setOnClickListener(this);
         tv_labellat = findViewById(R.id.tv_labellat);
@@ -107,28 +96,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tv_address = findViewById(R.id.tv_address);
         tv_lbladdress = findViewById(R.id.tv_lbladdress);
 
-
     } // end of onCreate method
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case PERMISSIONS_FINE_LOCATION: {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    updateGPS();
-                } else {
-                    Toast.makeText(this, "Hi Wogal No Permission Granted", Toast.LENGTH_SHORT).show();
-                    finish();
-                }
-            }
-
-        }
-    }
-
-    // *** GLOBAL onClick Listener ***
-    @Override
     public void onClick(View v) {
+
         if (v.getId() == R.id.sw_gps) {
             if (sw_gps.isChecked()) {
                 // most accurate
@@ -138,71 +110,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
                 tv_sensor.setText("Using Towers + Wifi");
             }
+        } else if (v.getId() == R.id.bu_TestCode) {
+            Toast.makeText(this,"Hi Test Butt",Toast.LENGTH_SHORT).show();
         }
         //else if (v.getId() == R.id.sw_locationsupdates) {}
 
     } // end of onClick method
 
-    private void updateGPS() {
-        // get permissions from user to track GPS
-        // get current location from the fused client
-        // update the UI - ie: set all properties in there associated text view items.
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
-//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-//            // we have user permission
-//            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-//                @Override
-//                public void onSuccess(Location location) {
-//                    // we got permission , put the values of location XXX into the UI components
-//                    updateUIValues(location);
-//                }
-//            });
-//
-//        } else {
-//            // permission NOT granted yet
-//            if (Build.VERSION.SDK_INT >= VERSION_CODES.M) {
-//                requestPermissions(new String[]{Manifest.permission.    .ACCESS_FINE_LOCATION}, PERMISSIONS_FINE_LOCATION);
-//            }
-    }
-
-
-//private void updateUIValues(Location location) {
-//    // we got permission , put the values of location XXX into the UI components
-//    // so update text view with new location
-//    tv_lat.setText(String.valueOf(location.getLatitude()));
-//    tv_lon.setText(String.valueOf(location.getLongitude()));
-//    tv_accuracy.setText(String.valueOf(location.getAccuracy()));
-//    if (location.hasAltitude()) {
-//        tv_altitude.setText(String.valueOf(location.getAltitude()));
-//    } else {
-//        tv_altitude.setText(R.string.not_available);
-//    }
-//    if (location.hasSpeed()) {
-//        tv_speed.setText(String.valueOf(location.getSpeed()));
-//    } else {
-//        tv_speed.setText(R.string.not_available);
-//    }
-//
-//
-//}
-
-} // end of MainActivity
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
